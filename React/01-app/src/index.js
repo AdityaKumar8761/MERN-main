94520451278a5f3f3577ac92266137290d675e4c
AdityaKumar8761 <S24CSEU1905@bennett.edu.in>
2025-09-10 22:41:10 +0530

    react2

--- React/01-app/src/index.js
+++ React/01-app/src/index.js
@@ -1,5 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+function HelloWorld(){ // this is a component in react(must return jsx and must start form capital letter)
+    return (
+    <div>
+        <h1>Hello form component</h1>
+        <p>
+            how are you doing hope you are fine
+        </p>
+    </div>
+    )  // jsx javascript XML
+}
+
+function MyComponent(){
+    const name = "Piyush Garg";
+    return <h2> Hello {name}</h2>; //html and java using jsx //you can run any valid javascript code in curly bracket
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<h1>Hello world</h1>)
+root.render(<HelloWorld/>)
+const component = ReactDOM.createRoot(document.getElementById('comp'))
+component.render(<MyComponent/>)
