75f8c37920057dc301510475078550bbbb1bee9b
AdityaKumar8761 <S24CSEU1905@bennett.edu.in>
2025-09-10 22:08:04 +0530

    react 2

--- React/01-app/src/index.js
+++ React/01-app/src/index.js
@@ -1,9 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import ReactDOM from 'react-dom/client';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<h1>Hello world</h1>)
